0d931745df5a3fa4d6ed21b98e12f985103a4f51
CHThia <capitoljazzpro@gmail.com>
2024-10-08 17:48:50 +0800

    Rectify and test 3D model format

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'aframe';
 import Box from "../src/assets/slide-box.glb"
-import Ball from "../src/assets/ball.glb"
+import Ball from "../src/assets/ball.obj"
+import BallM from "../src/assets/ball.mtl"
 
 
 export default function App() {
@@ -11,14 +12,15 @@ export default function App() {
           
           <a-assets>
             <a-asset-item id="box" src={ Box }></a-asset-item>
-            <a-asset-item id="ball" src={ Ball }></a-asset-item>
+            <a-asset-item id="ball-o" src={ Ball }></a-asset-item>
+            <a-asset-item id="ball-m" src={ BallM }></a-asset-item>
           </a-assets>
           
           <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
           <a-sky color="#bfdbff"></a-sky>
 
           <a-entity gltf-model="#box" scale="0.5 0.15 0.15" position="0 0 -4"></a-entity>
-          <a-entity gltf-model="#ball" scale="0.2 0.2 0.2" position="0 0 -4"></a-entity>
+          <a-entity obj-model="obj: #ball-o; mtl: #ball-m" scale="0.2 0.2 0.2" position="0 0 -4"></a-entity>
 
           <a-entity light="type: spot; intensity: 1" position="0 2.5 0"></a-entity>
 
