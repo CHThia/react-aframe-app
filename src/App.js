57ea00b2bc02e69fb2c218ea3c8a203056d3cfdd
CHThia <capitoljazzpro@gmail.com>
2024-10-08 19:44:22 +0800

    Update and explore methods to display imported 3D assets

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import 'aframe';
-// import Box from "../src/assets/slide-box.glb"
-// import Ball from "../src/assets/ball.obj"
-// import BallM from "../src/assets/ball.mtl"
+import Box from "../src/assets/slide-box.glb"
+import Ball from "../src/assets/ball.obj"
+import BallMaterial from "../src/assets/ball.mtl"
 
 
 export default function App() {
   return (
     <>
       <div className="App">
-        <a-scene>
 
-        <a-sphere position="0 1 -3" radius="0.75" color="#EF2D5E"></a-sphere>
-          
-        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+        <a-scene xr-mode-ui="enabled: true"> 
+
+        {/* <a-sphere position="0 1 -3" radius="0.75" color="#EF2D5E"></a-sphere> */}
+        
+        <a-assets>
+          <a-asset-item id="box" src={ Box }></a-asset-item>
+          <a-asset-item id="ball-obj" src={ Ball }></a-asset-item>
+          <a-asset-item id="ball-mtl" src={ BallMaterial }></a-asset-item>
+        </a-assets>
+        
+
         <a-sky color="#bfdbff"></a-sky>
+        <a-gltf-model src="#box" scale="0.5 0.15 0.15" position="0 0 -4" ></a-gltf-model>
+        <a-obj-model src="#ball-obj" mtl="#ball-mtl" scale="0.3 0.3 0.3" position="0 0 -4" ></a-obj-model>
+        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" ></a-plane>
 
-          {/* <a-assets>
-            <a-asset-item id="box" src={ Box }></a-asset-item>
-            <a-asset-item id="ball-o" src={ Ball }></a-asset-item>
-            <a-asset-item id="ball-m" src={ BallM }></a-asset-item>
-          </a-assets>
-          
 
-          <a-entity gltf-model="#box" scale="0.5 0.15 0.15" position="0 0 -4"></a-entity>
-          <a-entity obj-model="obj: #ball-o; mtl: #ball-m" scale="0.2 0.2 0.2" position="0 0 -4"></a-entity>
-
-          <a-entity light="type: spot; intensity: 1" position="0 2.5 0"></a-entity> */}
+        <a-entity light="type: ambient; color: #BBB" position="0 2.5 0"></a-entity>
+        <a-entity light="type: point; intensity: 1; distance: 80; decay: 1" position="10 30 10" ></a-entity>
 
         </a-scene>
+
       </div>
     </>
   );
