014002c8c917f334d65b71c7ab4e31d30cf20051
CHThia <capitoljazzpro@gmail.com>
2024-10-08 17:56:20 +0800

    Revert to simple 3D model setup as gltf or obj is not displaying in deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'aframe';
-import Box from "../src/assets/slide-box.glb"
-import Ball from "../src/assets/ball.obj"
-import BallM from "../src/assets/ball.mtl"
+// import Box from "../src/assets/slide-box.glb"
+// import Ball from "../src/assets/ball.obj"
+// import BallM from "../src/assets/ball.mtl"
 
 
 export default function App() {
@@ -9,20 +9,23 @@ export default function App() {
     <>
       <div className="App">
         <a-scene>
+
+        <a-sphere position="0 1 -3" radius="0.75" color="#EF2D5E"></a-sphere>
           
-          <a-assets>
+        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+        <a-sky color="#bfdbff"></a-sky>
+
+          {/* <a-assets>
             <a-asset-item id="box" src={ Box }></a-asset-item>
             <a-asset-item id="ball-o" src={ Ball }></a-asset-item>
             <a-asset-item id="ball-m" src={ BallM }></a-asset-item>
           </a-assets>
           
-          <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-          <a-sky color="#bfdbff"></a-sky>
 
           <a-entity gltf-model="#box" scale="0.5 0.15 0.15" position="0 0 -4"></a-entity>
           <a-entity obj-model="obj: #ball-o; mtl: #ball-m" scale="0.2 0.2 0.2" position="0 0 -4"></a-entity>
 
-          <a-entity light="type: spot; intensity: 1" position="0 2.5 0"></a-entity>
+          <a-entity light="type: spot; intensity: 1" position="0 2.5 0"></a-entity> */}
 
         </a-scene>
       </div>
