a0d5b2f2f8ca7f2d04577157b22d155ec41cdac3
CHThia <capitoljazzpro@gmail.com>
2024-10-08 17:30:25 +0800

    Update and replace scene with new 3D asset and light

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import 'aframe';
+import Box from "../src/assets/slide-box.glb"
+import Ball from "../src/assets/ball.glb"
+
 
 export default function App() {
   return (
     <>
       <div className="App">
         <a-scene>
-          <a-box position="0 1 -5" rotation="0 45 45" color="#4CC3D9"></a-box>
-          <a-sphere position="0 2 -3" radius="1.25" color="#EF2D5E"></a-sphere>
-          <a-cylinder position="1 0.75 -5" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+          
+          <a-assets>
+            <a-asset-item id="box" src={ Box }></a-asset-item>
+            <a-asset-item id="ball" src={ Ball }></a-asset-item>
+          </a-assets>
+          
           <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-          <a-sky color="#ECECEC"></a-sky>
+          <a-sky color="#bfdbff"></a-sky>
+
+          <a-entity gltf-model="#box" scale="0.5 0.15 0.15" position="0 0 -4"></a-entity>
+          <a-entity gltf-model="#ball" scale="0.2 0.2 0.2" position="0 0 -4"></a-entity>
+
+          <a-entity light="type: spot; intensity: 1" position="0 2.5 0"></a-entity>
+
         </a-scene>
       </div>
     </>
